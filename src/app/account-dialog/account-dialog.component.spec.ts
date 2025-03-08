f34f655138a7d83b31577d4fbd42079dd7fc057b
daenis-dev <daenis.dev@gmail.com>
2025-03-07 14:17:10 -1000

    Removed fdescribe

--- src/app/account-dialog/account-dialog.component.spec.ts
+++ src/app/account-dialog/account-dialog.component.spec.ts
@@ -10,7 +10,7 @@ import { of } from 'rxjs';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
-fdescribe('AccountDialogComponent', () => {
+describe('AccountDialogComponent', () => {
   let component: AccountDialogComponent;
   let fixture: ComponentFixture<AccountDialogComponent>;
   let authServiceSpy: jasmine.SpyObj<AuthService>;
