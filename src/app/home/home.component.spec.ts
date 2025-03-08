d5eb8936966bd430f6fe787b7e4727930c2c458e
daenis-dev <daenis.dev@gmail.com>
2025-03-07 14:20:41 -1000

    Home component test

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { Router } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ]
     })
     .compileComponents();
 
@@ -19,5 +26,15 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to "/my-backups" when openMyFiles is called', () => {
+    component.openMyFiles();
+    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('/my-backups');
+  });
+
+  it('should navigate to "/my-calendar" when openMyCalendar is called', () => {
+    component.openMyCalendar();
+    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('/my-calendar');
   });
 });
